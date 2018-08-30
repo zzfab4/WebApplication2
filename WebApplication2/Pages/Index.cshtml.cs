4ffb0d956c27fc03d881f9cdcd033e980d5723ca
zzfab4 <fabs2001@gmail.com>
2018-08-30 23:00:01 +1000

    add

--- WebApplication2/Pages/Index.cshtml.cs
+++ WebApplication2/Pages/Index.cshtml.cs
@@ -4,14 +4,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication2.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly AppDbContext _db;
 
+        public IndexModel(AppDbContext db) { _db = db; }
+
+        public IList<Customer> Customers { get; private set; }
+
+        public async Task OnGetAsync()
+        {
+            Customers = await _db.Customers.AsNoTracking().ToListAsync();
         }
     }
 }
