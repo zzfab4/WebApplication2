bb1257941809ba7e96805870d37b71e144bfe4f5
zzfab4 <fabs2001@gmail.com>
2018-08-30 22:37:25 +1000

    All

---
+++ WebApplication2/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2
+{
+    public class AppDbContext : DbContext
+    {
+        private static DbContextOptions options;
+
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ WebApplication2/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        [Required, StringLength(25)]
+        public string Name { get; set; }
+
+    }
+}
